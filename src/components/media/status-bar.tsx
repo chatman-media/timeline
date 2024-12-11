d49ab86509123616ae9b167d752b394ce6fa4792
Alexander Kireev <ak.chatman.media@gmail.com>
2024-12-11 13:50:41 +0700

    up

--- src/components/media/status-bar.tsx
+++ src/components/media/status-bar.tsx
@@ -30,7 +30,7 @@ export function StatusBar({
   return (
     <div className="flex justify-between items-start p-0 text-sm m-1">
       <div className="flex flex-col items-start gap-0 text-xs text-gray-500 dark:text-gray-500">
-        <span className="px-1 flex items-center whitespace-nowrap">
+        <span className="px-1 flex items-center whitespace-nowrap gap-1">
           <ActionButton title="Добавить все видео" onClick={onAddAllVideoFiles}>
             {media.filter((f) => f.probeData?.streams?.[0]?.codec_type === "video").length} видео
           </ActionButton>
